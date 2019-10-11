7678966aa330886fd80286e62f8a433bc7e2e166
Eun Yi <eunyi@minint-e3s17df.corporate.thoughtworks.com>
2019-10-11 17:00:53 -0500

    add MenuTest.class

---
+++ test/com/twu/biblioteca/MenuTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MenuTest {
+
+    @Test
+    public void displayMainMenu() {
+    }
+
+    @Test
+    public void runMenu() {
+    }
+}
